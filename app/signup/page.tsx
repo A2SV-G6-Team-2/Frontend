f5dc2f13dd0703936a855c8b0ffaf5f172a2854a
Hilcbek <balemayehu07@gmail.com>
2026-03-24 22:54:40 -0700

    feat: initial commit

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,12 @@
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <div className="max-w-3xl">
+        <div className="w-full bg-red-400"></div>
+        <div className="w-full">
+          
+        </div>
+      </div>
+    </div>
+  );
 }
